71800258e63d7ddca3700b0afda8f8771e1cb380
Richard Nedbalek <nedbalek.richard@gmail.com>
2015-12-05 00:55:07 +0100

    prepared functionality for new options; like --automatic and --location

--- archivr.py
+++ archivr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!rusr/bin/env python
 #
 ## archivr.py v0.9 | RN
 #
@@ -15,14 +15,17 @@ cflist="""
 /etc/fstab
 /etc/sudoers
 /etc/resolv.conf
+/etc/lvm/lvm.conf
 """\
 .split()
 
-# smtp configuration section
-smtpserver="smtp.server.example"
-sender="archivr <archivr-noreply@example.com>"
-receivers=','.join(["jane.doe@example.com"])
+# smtp configuration 
+smtpserver="smtp1.emea.omc.hp.com"
+sender="archivr <archivr-noreply@hpe.com>"
+receivers=','.join(["richard.nedbalek@hpe.com"])
 subject="archivr report | "+socket.getfqdn()
+# archive location
+archpath="/var/log/archivr"+"/"
 
 hname = socket.getfqdn()
 ctimestamp = datetime.datetime.now()
@@ -37,6 +40,16 @@ class handling():
 		print "err: %s" % text 
 	def f_ok(self,text):
 		print "ok: %s" % text
+	def f_createarchdir(self,archpath):
+		if os.path.isdir(archpath):
+			pass
+		else:
+			try: 
+				self.f_ok("directory does not exist, creating")
+				os.mkdir(archpath,0700)
+			except:
+				self.f_err("cannot create %s, check manually" % archpath)
+				sys.exit(1)
 	def f_filecheck(self,cflist):
 			for cfile in cflist:
 				try:
@@ -47,16 +60,19 @@ class handling():
 		if os.getgid() != 0:
 			handling().f_err("not root")
 			sys.exit(1)
-
+# end of class 'handling'
 class archive():
+	global archloc
+	archloc=archpath+cfilename
+	handling().f_createarchdir(archpath)
 	"""archive cflist - list of files"""
 	def __init__(self):
 		"self"
 	def f_addfiles(self,cflist): # TODO with
 		try:
-			tar = tarfile.open(cfilename, "a")
+			tar = tarfile.open(archloc, "a")
 		except (IOError, OSError):
-			handling().f_err("cannot open archive %s" % cfilename)
+			handling().f_err("cannot open archive %s" % archloc)
 			sys.exit(1)
 		for cfile in cflist:
 			try:
@@ -67,28 +83,27 @@ class archive():
 		try:
 			tar.close()
 		except (IOError, OSError):
-			handling().f_err("cannot close archive %s" % cfilename)
-
+			handling().f_err("cannot close archive %s" % archloc)
+# end of class 'archive'
 class check():
 	""" compare archived files with system files """
 	def __init__(self):
 		"self"
-
-        def f_email(self,message):
-		header="""From: %s 
-To: %s 
-Subject: %s \n""" % (sender,receivers,subject)
+        def f_email(self,message):# TODO: exceptions
+		header="From: %s \n" % sender
+		header+="To: %s \n" % receivers
+		header+="Subject: %s \n" % subject
 		msg=()
 		for key,value in message.iteritems():
 			msg+=key,value+("\n\n")
                 server = smtplib.SMTP(smtpserver)
-		# create tuple from header string and merge with msg - send that
+		# create a tuple from string variable 'header' and merge with 'msg' - send that
                 server.sendmail(sender, receivers, ''.join(((header,)+msg)))
                 server.quit()
-
 	def f_compare(self,cflist,archive):
 		diffdict={}
 		archive=''.join(archive)
+		archive=archpath+archive
 		# open archive && strip leading slash from each file path;
 		# tar doesnt want to deal with that shit
 		try:
@@ -123,20 +138,27 @@ Subject: %s \n""" % (sender,receivers,subject)
 			print ""
 		if args.email:
 			self.f_email(diffdict)
-
-
+# end of class 'check'
 parser = argparse.ArgumentParser(description='archive system config files for later integrity checks')
-parser.add_argument('-a', '--archive', action='store_true', help='archive defined cfg files')
-parser.add_argument('-c', '--check', nargs=1, metavar='<archive>', help='check/compare archived files with system files')
-parser.add_argument('-e', '--email', action='store_true', help='email diffs to a configured address'  )
+# some options are exclusive. like check and archive and automatic
+me1=parser.add_mutually_exclusive_group()
+me1.add_argument('-a', '--archive', action='store_true', help='archive defined cfg files')
+me1.add_argument('-c', '--check', nargs=1, metavar='<archive>', help='check/compare archived files with system files')
+parser.add_argument('-e', '--email', action='store_true', help='email diffs to a configured address')
+me1.add_argument('-t', '--automatic', action='store_true', help='autonomous mode - create a new archive snapshot and check last archived files in one go')
+parser.add_argument('-l', '--location', action='store_true', help='archive output location; default: /var/log/archivr ')
+
 args = parser.parse_args()
 if len(sys.argv) < 2:
 	parser.print_help()
 	sys.exit(1)
 # am I uid 0?
 handling().f_usercheck()
-if args.archive: 
+if args.archive:
 	archive().f_addfiles(cflist)
-
 if args.check:
 	check().f_compare(cflist,args.check)
+if args.automatic:
+	print "todo"
+if args.location:
+	print "todo"
